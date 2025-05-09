7d3f1dd24f19a1ae1b1ffe71312d94491570dbc9
you1880 <135771445+you1880@users.noreply.github.com>
2025-05-09 22:25:29 +0900

    UI 베이스 / UI 이벤트 핸들러 스크립트

---
+++ Assets/Scripts/UI/UI_Base.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public abstract class UI_Base : MonoBehaviour
+{
+    private Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+    
+    protected void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+
+        _objects.Add(typeof(T), objects);
+
+        for(int i = 0; i < names.Length; i++)
+        {
+            if(typeof(T) == typeof(GameObject))
+            {
+                objects[i] = Util.FindChild(gameObject, names[i], true);
+            }
+            else
+            {
+                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+            }
+
+            if(objects[i] == null)
+            {
+                Debug.Log($"Failed to Bind {names[i]}");
+            }
+        }
+    }
+
+    protected T Get<T>(int idx) where T : UnityEngine.Object
+    {
+        UnityEngine.Object[] objects = null;
+
+        if(_objects.TryGetValue(typeof(T), out objects) == false)
+        {
+            return null;
+        }
+
+        return objects[idx] as T;
+    }
+
+    protected GameObject GetObject(int idx) { return Get<GameObject>(idx); }
+    protected TextMeshProUGUI GetText(int idx) { return Get<TextMeshProUGUI>(idx); }
+    protected Image GetImage(int idx) { return Get<Image>(idx); }
+    protected Button GetButton(int idx) { return Get<Button>(idx); }
+
+    public static void BindEvent(GameObject obj, Action<PointerEventData> action, Define.UIEvent eventType = Define.UIEvent.Click)
+    {
+        UI_EventHandler eventHandler = Util.GetOrAddComponent<UI_EventHandler>(obj);
+        
+        switch(eventType)
+        {
+            case Define.UIEvent.Click:
+                eventHandler.OnClickHandler -= action;
+                eventHandler.OnClickHandler += action;
+                break;
+        }
+    }
+
+    public abstract void Init();
+
+    private void Start()
+    {
+        Init();
+    }
+}
