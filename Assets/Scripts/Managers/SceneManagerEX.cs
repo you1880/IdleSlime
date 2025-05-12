a5c643d11a4130a20074cea6a40d15efd0e6d6bb
you1880 <135771445+you1880@users.noreply.github.com>
2025-05-13 00:07:08 +0900

    사용자 정의 SceneManager 추가
    Define.SceneType을 이용하여 비동기 씬 이동
    UI_Blocker을 사용하여 Fade In / Out 관리

---
+++ Assets/Scripts/Managers/SceneManagerEX.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerEX
+{
+    private const float WAIT_TIME = 1.0f;
+    private const float FADE_DELAY_TIME = 1.0f;
+    private const float ALPHA_OPAQUE = 1.0f;
+    private const float ALPHA_TRANSPARENT = 0.0f;
+    private GameObject _blocker;
+    private Image _blockImage;
+    private UI_Blocker _blockerUI;
+
+    public void LoadNextScene(Define.SceneType sceneType = Define.SceneType.Main)
+    {
+        string sceneName = GetSceneName(sceneType);
+
+        Managers.RunCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private void CreateBlockerUI()
+    {
+        if(_blocker != null)
+        {
+            return;
+        }
+
+        _blockerUI = Managers.UI.ShowUI<UI_Blocker>();
+        _blocker = _blockerUI.gameObject;
+        _blockImage = _blocker.GetComponent<Image>();
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        float startTime = Time.time;
+        FadeOut();
+        SetBlockText("로딩중...");
+
+        while(operation.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        float elapsed = Time.time - startTime;
+
+        if(elapsed < WAIT_TIME)
+        {
+            yield return new WaitForSeconds(WAIT_TIME - elapsed);
+        }
+
+        SetBlockText("로딩 완료!");
+        yield return new WaitForSeconds(WAIT_TIME / 2.0f);
+
+        //yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        operation.allowSceneActivation = true;
+
+        FadeIn();
+    }
+
+    public IEnumerator Fade(float start, float end)
+    {
+        if(_blocker == null)
+        {
+            CreateBlockerUI();
+        }
+        
+        float elapsed = 0.0f;
+
+        Color c = _blockImage.color;
+        _blockImage.color = new Color(c.r, c.g, c.b, start);
+
+        while(elapsed < FADE_DELAY_TIME)
+        {
+            if(_blockImage == null)
+            {
+                yield break;
+            }
+
+            elapsed += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsed / FADE_DELAY_TIME);
+            float alpha = Mathf.Lerp(start, end, t);
+
+            _blockImage.color = new Color(c.r, c.g, c.b, alpha);
+
+            yield return null;
+        }
+    }
+
+    private void SetBlockText(string text)
+    {
+        if(_blockerUI == null)
+        {
+            return;
+        }
+
+        _blockerUI.SetText(text);
+    }
+
+    private void Clear()
+    {
+        Managers.Resource.Destroy(_blocker);
+
+        _blocker = null;
+        _blockerUI = null;
+        _blockImage = null;
+    }
+
+    private void FadeOut()
+    {
+        Managers.RunCoroutine(Fade(ALPHA_TRANSPARENT, ALPHA_OPAQUE));
+    }
+
+    private void FadeIn()
+    {
+        Managers.RunCoroutine(Fade(ALPHA_OPAQUE, ALPHA_TRANSPARENT));
+        Clear();
+    }
+
+    private string GetSceneName(Define.SceneType type)
+    {
+        string name = Enum.GetName(typeof(Define.SceneType), type);
+
+        return name;
+    }
+}
