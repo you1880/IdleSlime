e29d490209481ff1fb4b800305070e9a1567723f
you1880 <135771445+you1880@users.noreply.github.com>
2025-05-13 00:06:14 +0900

    ResourceManager 스크립트
    다른 프로젝트에서 사용하던 것 그대로 사용

---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager
+{
+    public T Load<T>(string path) where T : Object
+    {
+        if(typeof(T) == typeof(GameObject))
+        {
+            string name = path;
+            int index = name.LastIndexOf('/');
+
+            if(index >= 0)
+            {
+                name = name.Substring(index + 1);
+            }
+        }
+
+        return Resources.Load<T>(path);
+    }    
+
+    public GameObject Instantiate(string path, Transform parent = null)
+    {
+        GameObject original = Load<GameObject>($"Prefabs/{path}");
+
+        if(original == null)
+        {
+            Debug.Log($"Failed to Load Prefab : {path}");
+            return null;
+        }
+
+        GameObject obj = Object.Instantiate(original, parent);
+        obj.name = original.name;
+
+        return obj;
+    }
+
+    public void Destroy(GameObject obj, float time = 0.0f)
+    {
+        if(obj == null)
+        {
+            return;
+        }
+
+        Object.Destroy(obj, time);
+    }
+}
