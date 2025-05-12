f943d1e7707f910a28849eb82ebd35448e39bd4e
you1880 <135771445+you1880@users.noreply.github.com>
2025-05-12 23:58:35 +0900

    HEX string -> Color로 변환 함수 추가

--- Assets/Scripts/Utils/Util.cs
+++ Assets/Scripts/Utils/Util.cs
@@ -65,4 +65,14 @@ public class Util
 
         return null;
     }
+
+    public static Color GetColorFromHex(string hex)
+    {
+        if(ColorUtility.TryParseHtmlString(hex, out Color color))
+        {
+            return color;
+        }
+        
+        return Color.white;
+    }
 }
