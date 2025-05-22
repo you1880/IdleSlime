a228e3597a57bacd1a34f079c2fb7ceec9228864
you1880 <135771445+you1880@users.noreply.github.com>
2025-05-22 22:57:12 +0900

    활성화 / 비활성화 시 동작 추가

--- Assets/Scripts/Scene/MainScene.cs
+++ Assets/Scripts/Scene/MainScene.cs
@@ -1,48 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
-using Data.Slime;
+using Data.Save;
 using UnityEngine;
 
 public class MainScene : BaseScene
 {
-    private const float MIN_X = -7.0f;
-    private const float MAX_X = 7.0f;
-    private const float MIN_Y = -2.5f;
-    private const float MAX_Y = 1.5f;
+    private List<OwnedSlime> _ownedSlimes => Managers.Data.UserDataManager.CurrentSaveData.ownedSlimes;
+    private HashSet<int> _spawnedSlimeTypes = new HashSet<int>();
 
-    private List<OwnedSlime> ownedSlimes;
-    
     private void SetRandomSpawnPosition(GameObject slimeObject)
     {
-        float randomPosX = Random.Range(MIN_X, MAX_X);
-        float randomPosY = Random.Range(MIN_Y, MAX_Y);
+        IReadOnlyList<float> canMoveRanges = Managers.Game.GetMoveRanges();
+
+        float randomPosX = Random.Range(canMoveRanges[0], canMoveRanges[1]);
+        float randomPosY = Random.Range(canMoveRanges[2], canMoveRanges[3]);
 
         slimeObject.transform.position = new Vector3(randomPosX, randomPosY, 0.0f);
     }
-    
+
     private void SpawnSlimes()
     {
-        foreach(OwnedSlime ownedSlime in ownedSlimes)
+        foreach (OwnedSlime ownedSlime in _ownedSlimes)
         {
+            if (_spawnedSlimeTypes.Contains(ownedSlime.slimeType))
+            {
+                continue;
+            }
+
             GameObject slimeObj = Managers.Resource.Instantiate("Slime");
-            
-            if(slimeObj != null)
+
+            if (slimeObj != null)
             {
                 Slime slime = slimeObj.GetComponent<Slime>();
                 slime.SetSlimeType(ownedSlime.slimeType);
 
                 SetRandomSpawnPosition(slimeObj);
+                _spawnedSlimeTypes.Add(ownedSlime.slimeType);
             }
         }
     }
 
-    public override void Clear() {}
+    public override void Clear() { }
     public override void Init()
     {
         CurrentScene = Define.SceneType.Main;
-        
-        ownedSlimes = Managers.Data.CurrentSaveData.ownedSlimes;
 
         SpawnSlimes();
+    }
+
+    private void OnEnable()
+    {
+        Managers.Data.UserDataManager.OnUserDataChanged -= SpawnSlimes;
+        Managers.Data.UserDataManager.OnUserDataChanged += SpawnSlimes;
+
+        Managers.Game.CollectIdleIncome();
+    }
+
+    private void OnDisable()
+    {
+        Managers.Data.UserDataManager.OnUserDataChanged -= SpawnSlimes;
+        Managers.Data.UserDataManager.InitCurrentSavePlayTime();
+        Managers.Game.StopIdleIncomeRoutine();
     }
 }
