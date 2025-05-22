557d2479a7b158525ee4813ceff15669ed4623e8
you1880 <135771445+you1880@users.noreply.github.com>
2025-05-22 22:59:56 +0900

    정의 추가
    EffectSoundType : 효과음 정의
    SlimeType : 슬라임 타입 정의
    SkillType : 스킬 종류 정의
    GradeType : 슬라임 등급 정의

--- Assets/Scripts/Utils/Define.cs
+++ Assets/Scripts/Utils/Define.cs
@@ -30,10 +30,56 @@ public class Define
         Count, // Count용
     }
 
+    public enum EffectSoundType
+    {
+        Button,
+        Buy,
+        Grow,
+        Fail,
+        Touch,
+        Unlock
+    }
+
     public enum SlimeState
     {
         Idle,
         Move,
         Touch
+    }
+
+    public enum SlimeType
+    {
+        MinSlimeType = 0,
+        SlimeOne,
+        SlimeTwo,
+        SlimeThree,
+        SlimeFour,
+        SlimeFive,
+        SlimeSix,
+        SlimeSeven,
+        SlimeEight,
+        SlimeNine,
+        SlimeTen,
+        SlimeEleven,
+        SlimeTwelve,
+        MaxSlimeType = 13
+    }
+
+    public enum SkillType
+    {
+        UnlockSlimeType = 0,
+        UpgradeClickMoney = 1,
+        UpgradeIdleMoney = 2,
+        AddEnhancementChance = 3,
+        MaxSkillType = 4
+    }
+
+    public enum GradeType
+    {
+        GradeC,
+        GradeB,
+        GradeA,
+        GradeS,
+        GradeSP
     }
 }
